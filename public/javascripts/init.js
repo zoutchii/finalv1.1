bb844374551688294bc98273486725491e029d4f
zoutchii <hamzawi.zoutch@gmail.com>
2019-05-17 10:50:07 +0200

    first commit

---
+++ public/javascripts/init.js
@@ -0,0 +1,11 @@
+//if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /firebase/init.js');
+ // Initialize Firebase		AIzaSyC4u4031dCaX_BupYVBvQIBWdHX_EBbxw8
+var config = {
+	apiKey: "AIzaSyDkaWB9BN_XNMpM5g_rduzNd41StSyhVyE",
+	authDomain: "pfee-95e72.firebaseapp.com",
+	databaseURL: "https://pfee-95e72.firebaseio.com",
+	projectId: "pfee-95e72",
+	storageBucket: "pfee-95e72.appspot.com",
+	messagingSenderId: "693047971279"
+  };
+  firebase.initializeApp(config);
